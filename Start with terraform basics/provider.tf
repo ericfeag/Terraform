88e5f7484c1e799afe6c36e07e80d3cf3404eef5
ericfeag <117212644+ericfeag@users.noreply.github.com>
2023-02-09 19:27:51 +0100

    testing

--- Start with terraform basics/provider.tf
+++ Start with terraform basics/provider.tf
@@ -1,5 +1,5 @@
 provider "aws" {
-  access_key = "AKIA5BCCEP2J5ZD3MKNR"
-  secret_key = "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"
+  access_key = "AKIA5BCCEP2JRHDEL7VS"
+  secret_key = ""
   region = "us-east-2"
 }
