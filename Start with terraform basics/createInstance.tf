4fc45ee555f5dc163ec788284ba7a41f7b1df512
ericfeag <117212644+ericfeag@users.noreply.github.com>
2023-02-09 19:10:59 +0100

    provider

--- Start with terraform basics/createInstance.tf
+++ Start with terraform basics/createInstance.tf
@@ -1,9 +1,3 @@
-provider "aws" {
-  access_key = "AKIA5BCCEP2J5ZD3MKNR"
-  secret_key = "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"
-  region = "us-east-2"
-}
-
 resource "aws_instance" "MyFirstInstance" {
     ami = "ami-05803413c51f242b7"
     instance_type = "t2.micro"
---
+++ Start with terraform basics/provider.tf
@@ -0,0 +1,5 @@
+provider "aws" {
+  access_key = "AKIA5BCCEP2J5ZD3MKNR"
+  secret_key = "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"
+  region = "us-east-2"
+}
