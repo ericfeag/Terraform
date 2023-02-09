8584d89248bfda2c9972ac76fd6451f41ebd060c
ericfeag <117212644+ericfeag@users.noreply.github.com>
2023-02-09 19:56:30 +0100

    dfasfd

--- Start with terraform basics/createInstance.tf
+++ Start with terraform basics/createInstance.tf
@@ -1,4 +1,9 @@
-resource "aws_instance" "MyFirstInstnace" {
+resource "aws_instance" "MyFirstInstance" {
+  count = 3
   ami           = "ami-05692172625678b4e"
   instance_type = "t2.micro"
+
+  tags{
+    Name = "threedemoinstances"
+  }
 }
