fe8e98d7db317b1a177c02f96917e93be5981454
ericfeag <117212644+ericfeag@users.noreply.github.com>
2023-02-10 17:39:32 +0100

    security group

---
+++ terraformconcepts/demodata/securitygroup.tf
@@ -0,0 +1,20 @@
+data "aws_ip_ranges" "us_east_ip_range" {
+    regions = ["us-east-1", "us-east-2"]
+    services = ["ec2"]
+}
+
+resource "aws_security_group" "sg_custom_us_east" {
+    name = "sg_custom_us_east"
+
+    ingress = {
+        from_port = "443"
+        to_port = "433"
+        protocol = "tcp"
+        cidr_blocks = data.aws_ip_ranges.us_east_ip_range.cidr_blocks
+    }
+
+    tags = {
+        CreateDate = aws.aws_ip_ranges.us_east_ip_range.create_date
+        SyncToken = data.aws_ip_ranges.us_east_ip_range.sync_token 
+    }
+}
