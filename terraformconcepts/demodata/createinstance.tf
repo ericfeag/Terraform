c7d2438ef782b6fcdbbcc6e1c455865dcb0a7dbc
ericfeag <117212644+ericfeag@users.noreply.github.com>
2023-02-10 17:29:35 +0100

    fixing

--- terraformconcepts/demodata/createinstance.tf
+++ terraformconcepts/demodata/createinstance.tf
@@ -2,11 +2,11 @@ data "aws_availability_zones" "avilable" {}
 
 data "aws_ami" "latest-ubuntu" {
   most_recent = true
-  owner = ["099720109477"]
+  owners = ["099720109477"]
 
   filter {
     name = "name"
-    values = "ubuntu/images/hvm-ssd/ubuntu-xenial-16.04-amd64-server-*"
+    values = ["ubuntu/images/hvm-ssd/ubuntu-xenial-16.04-amd64-server-*"]
 
   }
   filter {
