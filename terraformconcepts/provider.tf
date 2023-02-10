b0cfb07d7b6b2f4050ca361d3ce41a5b0a5417fa
ericfeag <117212644+ericfeag@users.noreply.github.com>
2023-02-10 16:43:52 +0100

    testing errors

--- terraformconcepts/provider.tf
+++ terraformconcepts/provider.tf
@@ -1,5 +1,5 @@
-provider "aws"{
-    access_key = "${var.AWS_ACCESS_KEY}"
-    secret_key = "${var.AWS_SECRET_KEY}"
-    region = "${var.AWS_REGION}"
+provider "aws" {
+    access_key = "AKIA5BCCEP2J772N3S45"
+    secret_key = "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"
+    region = "us-east-2"
 }
