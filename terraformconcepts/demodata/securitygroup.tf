e1107ca937b6d22030f093a0e5a0cab096dfd65b
ericfeag <117212644+ericfeag@users.noreply.github.com>
2023-02-10 17:46:47 +0100

    errors

--- terraformconcepts/demodata/securitygroup.tf
+++ terraformconcepts/demodata/securitygroup.tf
@@ -1,20 +1,20 @@
-data "aws_ip_ranges" "us_east_ip_ranges" {
-    regions = ["us-east-1", "us-east-2"]
+data "aws_ip_ranges" "us_east_ip_range" {
+    regions = ["us-east-1","us-east-2"]
     services = ["ec2"]
 }
 
-resource "aws_security_group" "sg_custom_us_east" {
-    name = "sg_custom_us_east"
+resource "aws_security_group" "sg-custom_us_east" {
+    name = "custom_us_east"
 
-    ingress = {
+    ingress {
         from_port = "443"
-        to_port = "433"
+        to_port = "443"
         protocol = "tcp"
-        cidr_blocks = data.aws_ip_ranges.us_east_ip_range.cidr_blocks
+        cidr_blocks = slice(data.aws_ip_ranges.us_east_ip_range.cidr_blocks, 0, 50)
     }
 
     tags = {
-        CreateDate = data.aws.aws_ip_ranges.us_east_ip_ranges.create_date
-        SyncToken = data.aws_ip_ranges.us_east_ip_ranges.sync_token 
+        CreateDate = data.aws_ip_ranges.us_east_ip_range.create_date
+        SyncToken = data.aws_ip_ranges.us_east_ip_range.sync_token
     }
 }
