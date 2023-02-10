11cf7cd2fa087d59ffa609f58fa7dad058f2983c
ericfeag <117212644+ericfeag@users.noreply.github.com>
2023-02-10 15:20:03 +0100

    TerraformVariables

--- TerraformVariables/createinstance.tf
+++ TerraformVariables/createinstance.tf
@@ -1,9 +1,8 @@
 resource "aws_instance" "MyFirstInstance" {
-  count = 3
   ami           = "ami-05692172625678b4e"
   instance_type = "t2.micro"
 
   tags = {
-    Name = "demoinstance-${count.index}"
+    Name = "demoinstance"
   }
 }
