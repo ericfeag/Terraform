feff219ca7884b6ff5a67609b98876ccf0714b96
ericfeag <117212644+ericfeag@users.noreply.github.com>
2023-02-11 17:18:03 +0100

    sg and ec2

---
+++ terraformaws/vpc/securitygroup.tf
@@ -0,0 +1,23 @@
+resource "aws_security_group" "allow_levelup-ssh" {
+  name        = "allow_levelup-ssh"
+  description = "sg allows ssh connection"
+  vpc_id      = aws_vpc.levelupvpc.id
+
+  ingress {
+    from_port        = 22
+    to_port          = 22
+    protocol         = "tcp"
+    cidr_blocks      = ["0.0.0.0/0"]
+  }
+
+  egress {
+    from_port        = 0
+    to_port          = 0
+    protocol         = "-1"
+    cidr_blocks      = ["0.0.0.0/0"]
+  }
+
+  tags = {
+    Name = "allow_levelup-ssh"
+  }
+}
