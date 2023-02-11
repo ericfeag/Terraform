40acd4603acca07f6920438ec724afe0de8b5c30
ericfeag <117212644+ericfeag@users.noreply.github.com>
2023-02-11 16:51:07 +0100

    errors II

--- terraformaws/vpc/nat.tf
+++ terraformaws/vpc/nat.tf
@@ -1,25 +1,27 @@
-#Define External IP
+#Define External IP 
 resource "aws_eip" "levelup-nat" {
-    vpc = true
+  vpc = true
 }
 
 resource "aws_nat_gateway" "levelup-nat-gw" {
   allocation_id = aws_eip.levelup-nat.id
   subnet_id     = aws_subnet.levelupvpc-public-1.id
-  depends_on = [aws_internet_gateway.levelup-gw]
+  depends_on    = [aws_internet_gateway.levelup-gw]
 }
 
 resource "aws_route_table" "levelup-private" {
   vpc_id = aws_vpc.levelupvpc.id
-  route = {
-    cidr_block = "0.0.0.0/0"
-    gateway_id = aws_nat_gateway.levelup-nat-gw.id
+  route {
+    cidr_block     = "0.0.0.0/0"
+    nat_gateway_id = aws_nat_gateway.levelup-nat-gw.id
   }
+
   tags = {
-    Name = "levelupvpc-private"
+    Name = "levelup-private"
   }
 }
 
+# route associations private
 resource "aws_route_table_association" "level-private-1-a" {
   subnet_id      = aws_subnet.levelupvpc-private-1.id
   route_table_id = aws_route_table.levelup-private.id
--- terraformaws/vpc/vpc.tf
+++ terraformaws/vpc/vpc.tf
@@ -1,21 +1,22 @@
-#Create aws vpc
+#Create AWS VPC
 resource "aws_vpc" "levelupvpc" {
   cidr_block       = "10.0.0.0/16"
   instance_tenancy = "default"
-  enable_dns_support = "true"
+  enable_dns_support   = "true"
   enable_dns_hostnames = "true"
-  enable_classiclink = "false"
+  enable_classiclink   = "false"
 
   tags = {
-    Name = "levelup_vpc"
+    Name = "levelupvpc"
   }
 }
 
+# Public Subnets in Custom VPC
 resource "aws_subnet" "levelupvpc-public-1" {
-  vpc_id     = aws_vpc.levelupvpc.id
-  cidr_block = "10.0.1.0/24"
+  vpc_id                  = aws_vpc.levelupvpc.id
+  cidr_block              = "10.0.1.0/24"
   map_public_ip_on_launch = "true"
-  availability_zone = "us-east-2a"
+  availability_zone       = "us-east-2a"
 
   tags = {
     Name = "levelupvpc-public-1"
@@ -23,10 +24,10 @@ resource "aws_subnet" "levelupvpc-public-1" {
 }
 
 resource "aws_subnet" "levelupvpc-public-2" {
-  vpc_id     = aws_vpc.levelupvpc.id
-  cidr_block = "10.0.2.0/24"
+  vpc_id                  = aws_vpc.levelupvpc.id
+  cidr_block              = "10.0.2.0/24"
   map_public_ip_on_launch = "true"
-  availability_zone = "us-east-2b"
+  availability_zone       = "us-east-2b"
 
   tags = {
     Name = "levelupvpc-public-2"
@@ -34,21 +35,22 @@ resource "aws_subnet" "levelupvpc-public-2" {
 }
 
 resource "aws_subnet" "levelupvpc-public-3" {
-  vpc_id     = aws_vpc.levelupvpc.id
-  cidr_block = "10.0.3.0/24"
+  vpc_id                  = aws_vpc.levelupvpc.id
+  cidr_block              = "10.0.3.0/24"
   map_public_ip_on_launch = "true"
-  availability_zone = "us-east-2c"
+  availability_zone       = "us-east-2c"
 
   tags = {
     Name = "levelupvpc-public-3"
   }
 }
 
+# Private Subnets in Custom VPC
 resource "aws_subnet" "levelupvpc-private-1" {
-  vpc_id     = aws_vpc.levelupvpc.id
-  cidr_block = "10.0.4.0/24"
+  vpc_id                  = aws_vpc.levelupvpc.id
+  cidr_block              = "10.0.4.0/24"
   map_public_ip_on_launch = "false"
-  availability_zone = "us-east-2a"
+  availability_zone       = "us-east-2a"
 
   tags = {
     Name = "levelupvpc-private-1"
@@ -56,10 +58,10 @@ resource "aws_subnet" "levelupvpc-private-1" {
 }
 
 resource "aws_subnet" "levelupvpc-private-2" {
-  vpc_id     = aws_vpc.levelupvpc.id
-  cidr_block = "10.0.5.0/24"
+  vpc_id                  = aws_vpc.levelupvpc.id
+  cidr_block              = "10.0.5.0/24"
   map_public_ip_on_launch = "false"
-  availability_zone = "us-east-2b"
+  availability_zone       = "us-east-2b"
 
   tags = {
     Name = "levelupvpc-private-2"
@@ -67,16 +69,17 @@ resource "aws_subnet" "levelupvpc-private-2" {
 }
 
 resource "aws_subnet" "levelupvpc-private-3" {
-  vpc_id     = aws_vpc.levelupvpc.id
-  cidr_block = "10.0.6.0/24"
+  vpc_id                  = aws_vpc.levelupvpc.id
+  cidr_block              = "10.0.6.0/24"
   map_public_ip_on_launch = "false"
-  availability_zone = "us-east-2c"
+  availability_zone       = "us-east-2c"
 
   tags = {
     Name = "levelupvpc-private-3"
   }
 }
 
+# Custom internet Gateway
 resource "aws_internet_gateway" "levelup-gw" {
   vpc_id = aws_vpc.levelupvpc.id
 
@@ -85,28 +88,30 @@ resource "aws_internet_gateway" "levelup-gw" {
   }
 }
 
+#Routing Table for the Custom VPC
 resource "aws_route_table" "levelup-public" {
   vpc_id = aws_vpc.levelupvpc.id
-  route = {
+  route {
     cidr_block = "0.0.0.0/0"
     gateway_id = aws_internet_gateway.levelup-gw.id
   }
+
   tags = {
-    Name = "levelupvpc-public-1"
+    Name = "levelup-public-1"
   }
 }
 
-resource "aws_main_route_table_association" "levelup-public-1-a" {
-  subnet_id         = aws_subnet.levelupvpc-public-1.id
+resource "aws_route_table_association" "levelup-public-1-a" {
+  subnet_id      = aws_subnet.levelupvpc-public-1.id
   route_table_id = aws_route_table.levelup-public.id
 }
 
-resource "aws_main_route_table_association" "levelup-public-2-a" {
-  subnet_id         = aws_subnet.levelupvpc-public-2.id
+resource "aws_route_table_association" "levelup-public-2-a" {
+  subnet_id      = aws_subnet.levelupvpc-public-2.id
   route_table_id = aws_route_table.levelup-public.id
 }
 
-resource "aws_main_route_table_association" "levelup-public-3-a" {
-  subnet_id         = aws_subnet.levelupvpc-public-3.id
+resource "aws_route_table_association" "levelup-public-3-a" {
+  subnet_id      = aws_subnet.levelupvpc-public-3.id
   route_table_id = aws_route_table.levelup-public.id
 }
