82bfd3c84123e9edc5d254bcb177c1937dce9402
ericfeag <117212644+ericfeag@users.noreply.github.com>
2023-02-11 19:58:21 +0100

    rds

---
+++ terraaws2/awsrds/securitygroup.tf
@@ -0,0 +1,47 @@
+resource "aws_security_group" "allow-levelup-ssh" {
+  name        = "allow_levelup-ssh"
+  description = "sg allows ssh connection"
+  vpc_id      = aws_vpc.levelupvpc.id
+
+  ingress {
+    from_port        = 22
+    to_port          = 22
+    protocol         = "tcp"
+    cidr_blocks      = ["0.0.0.0/0"]
+  }
+
+  egress {
+    from_port        = 0
+    to_port          = 0
+    protocol         = "-1"
+    cidr_blocks      = ["0.0.0.0/0"]
+  }
+
+  tags = {
+    Name = "allow_levelup-ssh"
+  }
+}
+
+resource "aws_security_group" "allow-mariadb-" {
+  name        = "allow mariadb"
+  description = "sg for mariadb"
+  vpc_id      = aws_vpc.levelupvpc.id
+
+  ingress {
+    from_port        = 3306
+    to_port          = 3306
+    protocol         = "tcp"
+    security_groups = [aws_security_group.allow_levelup-ssh.id]
+  }
+
+  egress {
+    from_port        = 0
+    to_port          = 0
+    protocol         = "-1"
+    cidr_blocks      = ["0.0.0.0/0"]
+  }
+
+  tags = {
+    Name = "allow_mariadb"
+  }
+}
