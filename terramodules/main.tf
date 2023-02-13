be5995c6f8b33ccc24ba5cebe2bef13ca3020d5c
ericfeag <117212644+ericfeag@users.noreply.github.com>
2023-02-13 18:40:17 +0100

    module ec2

---
+++ terramodules/main.tf
@@ -0,0 +1,13 @@
+module "ec2_cluster" {
+  source = "github.com/terraform-aws-modules/terraform-aws-ec2-instance.git"
+
+  name = "my-cluster"
+  ami = "ami-0f40c8f97004632f9"
+  instance_type = "t2.micro"
+  subnet_id = "subnet-0d98e4cd382c764f2"
+
+  tags = {
+    Terraform = "true"
+    Environment = "dev"
+  }
+}
