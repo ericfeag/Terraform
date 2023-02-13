aabda0677a3d5e5685458fbb98224ae8502eb4fd
ericfeag <117212644+ericfeag@users.noreply.github.com>
2023-02-13 19:12:03 +0100

    custom_vpc module

--- terraaws2/elb/variables.tf
+++ terraaws2/elb/variables.tf
@@ -1,37 +1,64 @@
 variable "AWS_ACCESS_KEY" {
-    type = string
-    default = "AKIA5BCCEP2J772N3S45"
+    type        = string
+    default     = "AKIA5BCCEP2J772N3S45"
 }
-
-variable "AWS_SECRET_KEY" {}
 
 variable "AWS_REGION" {
-default = "us-east-2"
+    type        = string
+    default     = "us-east-2"
 }
 
-variable "Security_Group"{
-    type = list
-    default = ["sg-24076", "sg-90890", "sg-456789"]
+variable "vpcname" {
+  description = "Name to be used on all the resources as identifier"
+  type        = string
+  default     = ""
 }
 
-variable "AMIS" {
-    type = map
-    default = {
-        us-east-1 = "ami-0f40c8f97004632f9"
-        us-east-2 = "ami-05692172625678b4e"
-        us-west-2 = "ami-0352d5a37fb4f603f"
-        us-west-1 = "ami-0f40c8f97004632f9"
-    }
+variable "cidr" {
+  description = "The CIDR block for the VPC"
+  type        = string
+  default     = "0.0.0.0/0"
 }
 
-variable "PATH_TO_PRIVATE_KEY" {
-  default = "levelup_key"
+variable "instance_tenancy" {
+  description = "A tenancy option for instances launched into the VPC"
+  type        = string
+  default     = "default"
 }
 
-variable "PATH_TO_PUBLIC_KEY" {
-  default = "levelup_key.pub"
+variable "enable_dns_hostnames" {
+  description = "Should be true to enable DNS hostnames in the VPC"
+  type        = bool
+  default     = false
 }
 
-variable "INSTANCE_USERNAME" {
-  default = "ubuntu"
+variable "enable_dns_support" {
+  description = "Should be true to enable DNS support in the VPC"
+  type        = bool
+  default     = true
 }
+
+variable "enable_classiclink" {
+  description = "Should be true to enable ClassicLink for the VPC. Only valid in regions and accounts that support EC2 Classic."
+  type        = bool
+  default     = null
+}
+
+variable "enable_classiclink_dns_support" {
+  description = "Should be true to enable ClassicLink DNS Support for the VPC. Only valid in regions and accounts that support EC2 Classic."
+  type        = bool
+  default     = null
+}
+
+variable "enable_ipv6" {
+  description = "Requests an Amazon-provided IPv6 CIDR block."
+  type        = bool
+  default     = false
+}
+
+variable "vpcenvironment" {
+  description = "AWS VPC Environment Name"
+  type        = string
+  default     = "Development"
+}
+
